da0047ea15159929a44b619535ed85c5f3c8d163
Taizo_Ayase <zxcvbnmkind@gmail.com>
2013-05-18 05:23:12 +0900

    initial commit for this app
    beta version for Grid Scan Helper
    TODO:some mouse gesture is not ideal
    second click on grid needs double click

---
+++ app.rb
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+require 'sinatra'
+require 'ap'
+require 'haml'
+
+get '/' do
+  haml :top
+
+end
+
+post '/grids' do
+  @ver = params[:vertical].to_i
+  @hor = params[:horizontal].to_i
+  @total = @ver * @hor
+  haml :grids
+end
+
+def html(view)
+  File.read(File.join('views', "#{view.to_s}.html"))
+end

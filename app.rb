086869852db6ab4c82119736b45d8dd8420299bd
Taizo_Ayase <zxcvbnmkind@gmail.com>
2013-05-18 13:44:46 +0900

    Fix some bugs
    When enter 0, replaced by number '1'
    (emualation of vertical / horizontal scan mode)
    These 2 modes might be added as other mode,
    i.e., users select vertical, horizontal or 2D mode

--- app.rb
+++ app.rb
@@ -5,16 +5,18 @@ require 'haml'
 
 get '/' do
   haml :top
-
 end
 
 post '/grids' do
+  # get vertical/horizontal grid size
   @ver = params[:vertical].to_i
   @hor = params[:horizontal].to_i
-  @total = @ver * @hor
-  haml :grids
-end
 
-def html(view)
-  File.read(File.join('views', "#{view.to_s}.html"))
+  # zero is replaced by 1
+  @ver = 1 if @ver == 0
+  @hor = 1 if @hor == 0
+
+  @total = @ver * @hor
+
+  haml :grids
 end
